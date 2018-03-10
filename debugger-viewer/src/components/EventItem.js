39562d85b89c6bc4c7d7bd3e42771e28d426a74c
Kobi Kadosh <kobi.kadosh@gmail.com>
2018-03-10 09:13:55 +0200

    fixes for EventItem

--- debugger-viewer/src/components/EventItem.js
+++ debugger-viewer/src/components/EventItem.js
@@ -1,59 +1,51 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Pane } from 'evergreen-ui'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Pane } from 'evergreen-ui';
 
-const PageEvent = ({ event }) => (<div>
-  {event.properties.title}
-  </div>);
-const IdentifyEvent = ({ event }) => (<div>
-  {event.userId} ({event.traits.email})
-  </div>);
-const TrackEvent = ({ event }) => (<div>
-  {event.event}
-  </div>);
+const PageEvent = ({ event }) => <div>{event.properties.title}</div>;
+const IdentifyEvent = ({ event }) => (
+  <div>
+    {event.userId} ({event.traits.email})
+  </div>
+);
+const TrackEvent = ({ event }) => <div>{event.event}</div>;
 
 const typesMap = {
   page: PageEvent,
   track: TrackEvent,
-  identify: IdentifyEvent
+  identify: IdentifyEvent,
 };
 
-const EventItem = ({ event }) => {
-  const { type } = event;
+const EventItem = ({ type, ...other }) => {
   const EventName = typesMap[type];
 
-  return (<Pane is="button" {...{ alignItems: "center", borderBottom: "extraMuted",
-display
-:
-"flex",
-float
-:
-"left",
-height
-:
-104,
-justifyContent
-:
-"center",
-margin
-:
-32,
-width
-:
-160,
-   }}>
-    <div>V</div>
-    <span>{event.type}</span>
-    <span><EventName event={event} /></span>
-  </Pane>);
-}
+  return (
+    <Pane
+      is="button"
+      {...{
+        alignItems: 'center',
+        borderBottom: 'extraMuted',
+        display: 'flex',
+        float: 'left',
+        height: 104,
+        justifyContent: 'center',
+        margin: 32,
+        width: 160,
+      }}
+    >
+      <div>V</div>
+      <span>{type}</span>
+      <span>
+        <EventName event={other} />
+      </span>
+    </Pane>
+  );
+};
 
 EventItem.propTypes = {
-  event: PropTypes.shape({
     id: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     data: PropTypes.object.isRequired,
-  }).isRequired,
-}
+};
 
-export default EventItem
+export default EventItem;
